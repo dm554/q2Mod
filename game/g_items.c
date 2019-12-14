198a8247840d480fc60a785f11b234f00c66838f
dm554 <dm554@njit.edu>
2019-12-14 13:34:44 -0500

    Basic Lightsaber Functionality

--- game/g_items.c
+++ game/g_items.c
@@ -36,6 +36,11 @@ void Weapon_GrenadeLauncher (edict_t *ent);
 void Weapon_Railgun (edict_t *ent);
 void Weapon_BFG (edict_t *ent);
 
+//::Pridkett
+void Weapon_Sword(edict_t *ent);
+//!Pridkett
+
+
 gitem_armor_t jacketarmor_info	= { 25,  50, .30, .00, ARMOR_JACKET};
 gitem_armor_t combatarmor_info	= { 50, 100, .60, .30, ARMOR_COMBAT};
 gitem_armor_t bodyarmor_info	= {100, 200, .80, .60, ARMOR_BODY};
@@ -1538,6 +1543,36 @@ always owned, never in the world
 		0,
 /* precache */ "sprites/s_bfg1.sp2 sprites/s_bfg2.sp2 sprites/s_bfg3.sp2 weapons/bfg__f1y.wav weapons/bfg__l1a.wav weapons/bfg__x1b.wav weapons/bfg_hum.wav"
 	},
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//::Pridkett
+	/* weapon_sword
+	always owned, never in the world
+	*/
+	{
+		"weapon_sword",
+		NULL,
+		Use_Weapon,                             //How to use
+		NULL,
+		Weapon_Sword,                           //What the function is
+		"misc/w_pkup.wav",
+		NULL,
+		0,
+		"models/weapons/v_blast/tris.md2",      //The models stuff
+		"w_blaster",                                    //Icon to be used
+		"Sword",                                        //Pickup name
+		0,
+		0,
+		NULL,
+		IT_WEAPON,
+		NULL,
+		0,
+		"weapons/blastf1a.wav misc/lasfly.wav" //The sound of the blaster
+		//This is precached
+	},
+
+	//!Pridkett
+	////////////////////////////////////////////////////////////////////////////////////////////////
 
 	//
 	// AMMO ITEMS
